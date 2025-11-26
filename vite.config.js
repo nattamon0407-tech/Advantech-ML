1a8e34deb3968a4b2a2d47a73f288db43c673b1a
nattamon <nattamon0407@gmail.com>
2025-11-26 16:53:30 +0700

    Config: Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = '/Advantech-ML'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoName,
   plugins: [
     react()
   ],
